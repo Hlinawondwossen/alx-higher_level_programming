48f313aed89589af4b7ea412c92d99c365190521
hlinawondwossen <hlinawondwossen2022@gmail.com>
2023-11-27 12:22:03 -0800

    hello

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+text = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str1 = text[39:66]
+str2 = text[106:112]
+str3 = text[:6]
+concatenated_str = str1 + str2 + str3
+print(concatenated_str)
