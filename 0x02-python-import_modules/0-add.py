589880645d074f2c86306279c76ab5dc689c5c7a
hlinawondwossen <hlinawondwossen2022@gmail.com>
2023-11-30 12:35:43 -0800

    add task

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,7 @@
-#!/usr/bin/python3
-from add_0 import add
-
+!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from add_0 import add
 a = 1
 b = 2
-
-import add_0
-
-result = add_0.add(a, b)
-print("{} + {} = {}".format(a, b, result))
+print("{} + {} = {}".format(a, b, add(a, b)))
