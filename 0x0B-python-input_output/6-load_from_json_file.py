32ad2490505cb946404af19d4aff072f4cb790ac
hlinawondwossen <Hlinawondwossen2022@gmail.com>
2024-01-09 15:55:11 -0800

    nme

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,9 @@
-"""Defines a function that creates an Object from a JSON file."""
-
+#!/usr/bin/python3
+"""This module defines a JSON file-reading function"""
 import json
 
 
 def load_from_json_file(filename):
-    """Creates an object from a JSON file."""
-    with open(filename, 'r', encoding='utf-8') as myFile:
-        return json.load(myFile)
+    """Creates a Python object from a given JSON file"""
+    with open(filename) as f:
+        return json.load(f)
