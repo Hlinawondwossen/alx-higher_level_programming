c3f919ad6454f636542a235f8a6645991a3e78dd
Hlinawondwossen <hlinawondwossen2022@gmail.com>
2024-03-28 13:46:06 -0700

    8-json_api.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL
-- displays the body of the response (decoded in utf-8).
+
 """
+- takes in a URL
+- sends a request to the URL
+- displays the body of the response (decoded in utf-8)
+"""
+
+import sys
+import urllib.parse
+import urllib.request
 
 
 if __name__ == "__main__":
@@ -14,4 +19,4 @@ if __name__ == "__main__":
         with request.urlopen(sys.argv[1]) as res:
             print(res.read().decode('UTF-8'))
     except error.HTTPError as er:
-        print('Error code:', er.code))
+        print('Error code:', er.code)
